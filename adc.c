008b8129e107efc11376757dd7ad0891116deea4
Piotr Knutel <pk1@linux.pl>
2022-08-19 13:45:10 +0200

    poprawianie konfiguracji ADC 1, do 'wyboru trybu wej ADC'. Wg przyk³adu 22-1 z ref. manual section 22

--- adc.c
+++ adc.c
@@ -2,19 +2,53 @@
 
 #include <xc.h>
 #include "adc.h"
+#include "p32mk1024mcf064.h"
 #include <stdio.h>
 #include <stdlib.h>
 
 void ADC_init() {
-    /* ADC 1 */
-    /* Procedura z noty, pocz?wszy od str. 371 */
-    ADC1CFG = DEVADC1;
-    ADCCON1bits.AICPMPEN = 0;   // bo Vdd =< 2.5V
-    CFGCONbits.IOANCPEN = 0;    // bo Vdd =< 2.5V
-    ADCCON1bits.ON = 0;         // wy?. ADC
+    /* ADC AN 1 */
+    /* Procedura z noty, poczawszy od str. 371 
+     * i z "reference manual" section 22 (w tym przyk??d 22-1 str. 66). */
     
-    /* Pozosta?? bity ADCCON1 nie zmieniane */
+    ADC1CFG = DEVADC1;
+    
+    /* Konfiguracja ADCCON 1 */
+    ADCCON1 = 0;                    // bez funkcji z tego rejestru, w tym bit ON=1 (ADC wy?)
+    
+    ADCCON1bits.AICPMPEN = 0;       // bo Vdd =< 2.5V
+    CFGCONbits.IOANCPEN = 0;        // bo Vdd =< 2.5V
+    
+    /* Inicjalizacja "warm up time" - ADCANCON */
+    ADCANCON = 0;                   // w tym bity ANENx musz? by? 0
+    ADCANCONbits.WKUPCLKCNT = 5;    // 5=2^5=32*Tad, po tylku taktach ADC b?dzie móg? konwertowa? 
+    
+    /* Ustawienie zegara - ADCCON 3 */
+    ADCCON3 = 0;                    // w tym DIGENx musi by? 0
+    ADCCON3bits.ADCSEL = 0;         // zródlo takowania ADC (Tclk): 0=PBCLK, 3=SystemClock
+    ADCCON3bits.CONCLKDIV = 1;      // Tq=Tclk, dzielnik takowania, 1=polowa
+    ADCCON3bits.VREFSEL = 0;        // wybór wej. dla Vref, 0=[AVdd i AVss]
+    
+    /* Ustawienie czasu próbowania i zegara konwersji - ADCxTIME */
+    /* ADCx, gdzie x to nr dedykowanego ADC */
+    ADC1TIMEbits.ADCEIS = 0;        // Wczesne przewanie ADC 1 jest generowane 1 takt zegara ADC przed ko?cem konwersji, je?li ADCEIS=8 to 8 cykli...
+    ADC1TIMEbits.SELRES = 3;        // rozdzielczosc 3=12 bitów
+    //ADC1TIMEbits.DMAEN = 0;         // DMA 0=wy?. (tego nie rozpozanawa? kompliator)
+    ADC1TIMEbits.ADCDIV = 1;        // 1=Tad=2*Tq, dzielnik taktowania ADC
+    ADC1TIMEbits.SAMC = 5;          // czas próbkowania, 0x3FF=1025*Tad, 5=5*Tad
+    
+    /* Ustawienie pinów jako wej?? do konkretnych ADC */
+    ADCTRGMODEbits.SH1ALT = 0;      // jesli 0 to AN1 jest wej ADC 1 (VBAT_ADC), te bity umo?liwiaj? po??czenie innego pinu z ADC1 
 
+    /* Wybór trybu wej ADC */
+    ADCIMCON1bits.DIFF1 = 0;        // je?li 0 to AN1 u?ywa trybu Single-ended
+    ADCIMCON1bits.SIGN1 = 0;        // je?li 0 to AN1 u?ywa trybu unsigned data
+    
+    
+    
+    
+    
+    
     ADCCON2bits.CVDCPL = 0b111; // Kondensator CVD = 17.5pF
     ADCCON2bits.SAMC = 0;   // (wa?ne) 0x3FF=1025*Tad, 0=2*Tad, dla Shared_ADC
     ADCCON2bits.BGVRIEN = 0;    // brak generowania przerwa?, gdy BGVRRDY=1
@@ -24,40 +58,12 @@ void ADC_init() {
     //ADCCON2bits.ECRIEN = 1;     // mo?liwe uruchomienie ADC z zewn?trznego modu?u, np. PTG (Peripherial Trigger Generator)
     ADCCON2bits.ADCEIS = 0b000; // Wczesne przewanie Shared_ADC jest generowane 1 takt zegara ADC przed ko?cem konwersji
     ADCCON2bits.ADCDIV = 0x7F;  // (wa?ne) Tad=254*Tq, taktowanie dla Shared_ADC=taktowanie_ADC/254
-
-    ADCANCONbits.ANEN0 = 0;
-    ADCANCONbits.ANEN1 = 0;
-    ADCANCONbits.ANEN2 = 0;
-    ADCANCONbits.ANEN3 = 0;
-    ADCANCONbits.ANEN4 = 0;
-    ADCANCONbits.ANEN5 = 0;
-    ADCANCONbits.ANEN7 = 0;
-
-    ADCCON3bits.DIGEN0 = 0;
-    ADCCON3bits.DIGEN1 = 0;
-    ADCCON3bits.DIGEN2 = 0;
-    ADCCON3bits.DIGEN3 = 0;
-    ADCCON3bits.DIGEN4 = 0;
-    ADCCON3bits.DIGEN5 = 0;
-    ADCCON3bits.DIGEN7 = 0;
-
-    ADCCON3bits.ADCSEL = 0;         // (wa?ne) ?ród?o takowania ADC (Tclk): 0=PBCLK, 3=SystemClock
-    ADCCON3bits.CONCLKDIV = 0x00;   // (wa?ne) Tq=Tclk, dzielnik takowania
-    ADCCON3bits.VREFSEL = 0;        // (wa?ne) wybór wej. dla Vref, 0=[AVdd i AVss]
-    /* pozosta?e bity ADCCON3 nie zmieniane */
-
-    ADC1TIMEbits.ADCEIS = 0;        // Wczesne przewanie ADC 1 jest generowane 1 takt zegara ADC przed ko?cem konwersji, je?li ADCEIS=8 to 8 cykli...
-    ADC1TIMEbits.SELRES = 0b11;     // rozdzielczo?? 12 bitów
-    //ADC1TIMEbits.DMAEN = 0;         // DMA 0=wy?.
-    ADC1TIMEbits.ADCDIV = 1;        // (wa?ne) Tad=2*Tq, dzielnik taktowania ADC
-    ADC1TIMEbits.SAMC = 0x3FF;      // (wa?ne) czas próbkowania, 0x3FF = 1025*Tad
     
-    ADCTRGMODEbits.SH1ALT = 0;      // je?li 0 to AN1 jest wej?ciem ADC 1 (VBAT), te bity umo?liwiaj? po??czenie innego pinu z ADC1 
+    
     ADCTRGMODEbits.STRGEN1 = 0;     // je?li 0 to nie u?ywa presynchronized triggers
     ADCTRGMODEbits.SSAMPEN1 = 0;    // je?li 0 to nie u?ywa synchronous sampling
     
-    ADCIMCON1bits.DIFF1 = 0;        // je?li 0 to AN1 u?ywa trybu Single-ended
-    ADCIMCON1bits.SIGN1 = 0;        // je?li 0 to AN1 u?ywa trybu unsigned data
+
 
     /* Rejestr ADCTRGSNS nie zmieniany. Wyszko bity 0. 
      Wej?cia analogowe reaguja na bocze narastaj?ce. */
